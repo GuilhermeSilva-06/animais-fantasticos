b7808d394fd4f14c476270539aceb5957ff98e25
Guilherme Fernades <gui.silva021007@gmail.com>
2026-02-26 16:32:25 -0300

    accordion tranformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,14 +1,18 @@
-export default function initAccordion() {
+export default class Accordion {
+  constructor(list){
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo'
+  }
   const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
 
   function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+    this.classList.toggle(this.activeClass);
+    this.nextElementSibling.classList.toggle(this.activeClass);
   }
 
   if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
+    accordionList[0].classList.add(this.activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
     accordionList.forEach((item) => {
